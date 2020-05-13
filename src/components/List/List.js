499488442e143efd85ec04f4a1d201b657c83c72
Kamil Bogucki <boguckikamil@hotmail.com>
2020-05-13 12:27:04 +0200

    wrap components inside container

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,13 +2,13 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero.js';
 import Column from '../Column/ColumnContainer.js';
+import Container from '../Container/Container.js';
 import PropTypes from 'prop-types';
 import { settings } from '../../data/dataStore';
 import ReactHtmlParser from 'react-html-parser';
 import Creator from '../Creator/Creator.js';
 
 class List extends React.Component {
-
   static propTypes = {
     title: PropTypes.node.isRequired,
     description: PropTypes.node,
@@ -22,23 +22,24 @@ class List extends React.Component {
   };
 
   render() {
-    const { title, image, description, columns, addColumn} = this.props;
+    const { title, image, description, columns, addColumn } = this.props;
     return (
-      <section className={styles.component}>
-        <Hero titleText={title} image={image} />
-        <div className={styles.description}>{ReactHtmlParser(description)}</div>
-        <div className={styles.columns}>
-          {columns.map((columnData) => (
-            <Column key={columnData.id} {...columnData} />
-          ))}
-        </div>
-        <div className={styles.creator}>
-          <Creator
-            text={settings.columnCreatorText}
-            action={addColumn}
-          />
-        </div>
-      </section>
+      <Container>
+        <section className={styles.component}>
+          <Hero titleText={title} image={image} />
+          <div className={styles.description}>
+            {ReactHtmlParser(description)}
+          </div>
+          <div className={styles.columns}>
+            {columns.map((columnData) => (
+              <Column key={columnData.id} {...columnData} />
+            ))}
+          </div>
+          <div className={styles.creator}>
+            <Creator text={settings.columnCreatorText} action={addColumn} />
+          </div>
+        </section>
+      </Container>
     );
   }
 }
